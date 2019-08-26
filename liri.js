a7c7fd95c45ef3c4ac73ad901a71ac59a2bdb4f2
Max Reinmueller <maxreinmueller@gmail.com>
2019-08-26 16:11:01 -0700

    specified console logs, moment.js not working

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var axios = require("axios");
 var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 
+
 // THE APP --------------------------------------------------------
 var userCommand = process.argv[2];
 switch (userCommand) {
@@ -25,28 +26,9 @@ switch (userCommand) {
 
 // FUNCTIONS -------------------------------------------------------
 
-// CONCERT bands in town
-function concertThis() {
-    var band = process.argv[3]
-
-    axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(
-        function(res){
-            if (band === undefined){
-                console.log("ya forgot to put in a band!")
-            } else {
-                console.log("Next Show For " + band);
-                console.log(res.data[0].venue.name);
-                console.log(res.data[0].venue.city);
-                console.log(res.data[0].datetime);
-
-            }
-        })
-        .catch(function(err){
-            if (err.res) {
-                console.log("Error", error.message);
-            }
-            console.log(err.config);
-        });
+// SPOTIFY
+function spotifyThis() {
+    console.log('spotify this');
 }
 
 // DO WHAT IT SAYS
@@ -54,10 +36,29 @@ function doWhatItSays() {
     console.log('do what it says');
 }
 
+// CONCERT bands in town
+function concertThis() {
+    var band = process.argv[3]
 
-// SPOTIFY
-function spotifyThis() {
-    console.log('spotify this');
+    axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(
+            function (res) {
+                if (band === undefined) {
+                    console.log("ya forgot to put in a band!")
+                } else {
+                    console.log("Next Show For " + band);
+                    console.log("venue: " + res.data[0].venue.name);
+                    console.log("City: " + res.data[0].venue.city);
+                    console.log("Date: " + res.data[0].datetime);
+                    var date = res.data[0].datetime;
+                    console.log("Moment.js date: UNDEFINED" );
+                }
+            })
+        .catch(function (err) {
+            if (err.res) {
+                console.log("Error", error.message);
+            }
+            console.log(err.config);
+        });
 }
 
 // MOVIE
